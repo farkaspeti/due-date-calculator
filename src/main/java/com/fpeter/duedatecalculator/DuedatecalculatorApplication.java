895b43e13803f46e75838e9031eeb9540011709b
Peter3 Farkas <peter4.farkas.ext@evosoft.com>
2021-06-22 10:50:15 +0200

    base project created via Spring Initializr

---
+++ src/main/java/com/fpeter/duedatecalculator/DuedatecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fpeter.duedatecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuedatecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuedatecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpeter/duedatecalculator/DuedatecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpeter.duedatecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuedatecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
