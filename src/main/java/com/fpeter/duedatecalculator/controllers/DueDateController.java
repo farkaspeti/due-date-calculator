6b1fe2779501b0ebabbc71a1b90eeea393d96d61
Peter3 Farkas <peter4.farkas.ext@evosoft.com>
2021-06-23 00:31:21 +0200

    DueDateController got implemented and out commented(Services are not implemented, I try to do it with TDD approach :D)

--- src/main/java/com/fpeter/duedatecalculator/controllers/DueDateController.java
+++ src/main/java/com/fpeter/duedatecalculator/controllers/DueDateController.java
@@ -1,5 +1,26 @@
 package com.fpeter.duedatecalculator.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("value = /duedate")
 public class DueDateController {
+/*	
+	private final DueDateService dueDateService;
 	
+	public DueDateController(DueDateService dueDateService) {
+		this.dueDateService = dueDateService;
+	}
+	
+	 @GetMapping(value = "/time/turnaround", produces = "application/json")	
+	 public ResponseEntity<DueDateDto> getDueDate(@RequestParam Calendar time @DateTimeFormat(pattern="MM/dd/yyyy"),
+	 											  @RequestParam int turnaround) throws Exception {
+	 	return dueDateService.CalculateDueDate(time, turnaround);
+	 }
+	    */
 }
