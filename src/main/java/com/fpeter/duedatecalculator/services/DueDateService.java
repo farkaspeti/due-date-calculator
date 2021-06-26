a57ee6d9d8ad80c2b91b3fb4c9fd6da211392e2f
z004593D <peter4.farkas@evosoft.com>
2021-06-26 16:44:02 +0200

    DuaDateService.java interface got implemented

--- src/main/java/com/fpeter/duedatecalculator/services/DueDateService.java
+++ src/main/java/com/fpeter/duedatecalculator/services/DueDateService.java
@@ -1,5 +1,8 @@
 package com.fpeter.duedatecalculator.services;
 
+import java.util.Calendar;
+
 public interface DueDateService {
 
+	Calendar CalculateDueDate(Calendar submitDate, int turnaround) throws DueDateServiceException;
 }
