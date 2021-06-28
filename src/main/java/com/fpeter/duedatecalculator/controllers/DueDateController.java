2640d83bc6e3323058460e15c3d1c272d6c368b5
z004593D <peter4.farkas@evosoft.com>
2021-06-28 21:54:26 +0200

    DueDateController got uncommented, getDueDate parameter list got fixed

--- src/main/java/com/fpeter/duedatecalculator/controllers/DueDateController.java
+++ src/main/java/com/fpeter/duedatecalculator/controllers/DueDateController.java
@@ -1,16 +1,21 @@
 package com.fpeter.duedatecalculator.controllers;
 
+import java.util.Calendar;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fpeter.duedatecalculator.dtos.DueDateDto;
+import com.fpeter.duedatecalculator.services.DueDateService;
 
 @RestController
 @RequestMapping("value = /duedate")
 public class DueDateController {
-/*	
+	
 	private final DueDateService dueDateService;
 	
 	public DueDateController(DueDateService dueDateService) {
@@ -18,9 +23,8 @@ public class DueDateController {
 	}
 	
 	 @GetMapping(value = "/time/turnaround", produces = "application/json")	
-	 public ResponseEntity<DueDateDto> getDueDate(@RequestParam Calendar time @DateTimeFormat(pattern="MM/dd/yyyy"),
-	 											  @RequestParam int turnaround) throws Exception {
+	 public ResponseEntity<DueDateDto> getDueDate(@RequestParam ("time") @DateTimeFormat(pattern="MM/dd/yyyy") Calendar time,
+	 											  @RequestParam ("turnaround") int turnaround) throws Exception {
 	 	return dueDateService.CalculateDueDate(time, turnaround);
 	 }
-	    */
 }
