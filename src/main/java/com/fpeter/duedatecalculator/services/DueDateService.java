2e5e924a511cb5d2b6e5be89fc019aff77df4670
z004593D <peter4.farkas@evosoft.com>
2021-06-28 21:57:53 +0200

    calculateDueDate method now returns with ResponseEntity<DueDateDto>

--- src/main/java/com/fpeter/duedatecalculator/services/DueDateService.java
+++ src/main/java/com/fpeter/duedatecalculator/services/DueDateService.java
@@ -2,9 +2,12 @@ package com.fpeter.duedatecalculator.services;
 
 import java.util.Calendar;
 
+import org.springframework.http.ResponseEntity;
+
+import com.fpeter.duedatecalculator.dtos.DueDateDto;
 import com.fpeter.duedatecalculator.exceptions.DueDateServiceException;
 
 public interface DueDateService {
 
-	Calendar CalculateDueDate(Calendar submitDate, int turnaround) throws DueDateServiceException;
+	public ResponseEntity<DueDateDto> CalculateDueDate(Calendar submitDate, int turnaround) throws DueDateServiceException;
 }
